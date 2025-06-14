9154ba31c11e3f139e2ab24a90315942f5d671bd
xiangnan <sxn123@vip.qq.com>
2025-06-14 19:24:36 +0800

    频率限制

--- routes/api.php
+++ routes/api.php
@@ -20,16 +20,32 @@ use App\Http\Controllers\Api\UsersController;
 // });
 
 
-Route::prefix('v1')->name('api.v1.')->group(function() {
-    Route::get('version', function() {
-        // abort(403, 'test');
-        return 'this is version v1';
-    })->name('version');
-    // 短信验证码
-    Route::post('verificationCodes', [VerificationCodesController::class, 'store'])
-        ->name('verificationCodes.store');
+Route::prefix('v1')
+    ->name('api.v1.')
+    ->group(function() {
+        Route::middleware('throttle:' . config('api.rate_limits.sign'))
+            ->group(function() {
+                // 短信验证码
+                Route::post('verificationCodes', [VerificationCodesController::class, 'store'])
+                    ->name('verificationCodes.store');
+                // 用户注册
+                Route::post('users', [UsersController::class, 'store'])
+                    ->name('users.store');
+            });
+        
+        Route::middleware('throttle:' . config('api.rate_limits.access'))
+            ->group(function() {
+                
+            });
 
-    // 用户注册
-    Route::post('users', [UsersController::class, 'store'])
-        ->name('users.store');
-});
+
+
+
+        // Route::get('version', function() {
+        //     // abort(403, 'test');
+        //     return 'this is version v1';
+        // })->name('version');
+        
+
+        
+    });
