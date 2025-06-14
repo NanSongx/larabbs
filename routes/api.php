f67bc85a99e440e8ccbb8d30fc6c1c99835e3bd2
xiangnan <sxn123@vip.qq.com>
2025-06-14 19:18:33 +0800

    用户注册

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\VerificationCodesController;
-
+use App\Http\Controllers\Api\UsersController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -28,4 +28,8 @@ Route::prefix('v1')->name('api.v1.')->group(function() {
     // 短信验证码
     Route::post('verificationCodes', [VerificationCodesController::class, 'store'])
         ->name('verificationCodes.store');
+
+    // 用户注册
+    Route::post('users', [UsersController::class, 'store'])
+        ->name('users.store');
 });
