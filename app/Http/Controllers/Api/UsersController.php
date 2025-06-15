219d6836f81d69470c64a8a39299e1f23cab55c1
xiangnan <sxn123@vip.qq.com>
2025-06-16 00:25:50 +0800

    编辑用户信息

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -8,6 +8,7 @@ use App\Http\Resources\UserResource;
 use App\Http\Requests\Api\UserRequest;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Support\Facades\Cache;
+use App\Models\Image;
 
 class UsersController extends Controller
 {
@@ -46,5 +47,22 @@ class UsersController extends Controller
     {
         // return new UserResource($request->user());
         return (new UserResource($request->user()))->showSensitiveFields();
+    }
+
+    public function update(UserRequest $request) 
+    {
+        $user = $request->user();
+        
+        $attributes = $request->only(['name', 'email', 'introduction']);
+
+        if ($request->avatar_image_id) {
+            $image = Image::find($request->avatar_image_id);
+
+            $attributes['avatar'] = $image->path;
+        }
+
+        $user->update($attributes);
+
+        return (new UserResource($user))->showSensitiveFields();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\VerificationCodesController;
 use App\Http\Controllers\Api\AuthorizationsController;
 use App\Http\Controllers\Api\UsersController;
+use App\Http\Controllers\Api\ImagesController;
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -65,6 +68,13 @@ Route::prefix('v1')
                     // 当前登录用户信息
                     Route::get('user', [UsersController::class, 'me'])
                         ->name('user.show');
+                    // 编辑登录用户信息
+                    Route::patch('user', [UsersController::class, 'update'])   
+                        ->name('user.update');
+                    // 上传图片
+                    Route::post('images', [ImagesController::class, 'store'])
+                        ->name('images.store');
+                    
                 });
             });
 
