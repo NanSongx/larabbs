b248a482db6bddf4b58ce67806332a619b310eee
xiangnan <sxn123@vip.qq.com>
2025-06-16 00:08:17 +0800

    用户信息

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -34,6 +34,17 @@ class UsersController extends Controller
         // 清除验证码缓存
         Cache::forget($cacheKey);
 
+        return (new UserResource($user))->showSensitiveFields();
+    }
+
+    public function show(User $user, Request $request)
+    {
         return new UserResource($user);
+    }
+
+    public function me(Request $request)
+    {
+        // return new UserResource($request->user());
+        return (new UserResource($request->user()))->showSensitiveFields();
     }
 }
