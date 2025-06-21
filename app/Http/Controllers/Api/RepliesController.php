12d935e4db4a80be46d4a5eb8f7ef0ebc5a1c992
xiangnan <sxn123@vip.qq.com>
2025-06-22 01:13:33 +0800

    删除回复

---
+++ app/Http/Controllers/Api/RepliesController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Topic;
+use App\Models\Reply;
+use Illuminate\Http\Request;
+use App\Http\Resources\ReplyResource;
+use App\Http\Requests\Api\ReplyRequest;
+
+class RepliesController extends Controller
+{
+    public function store(ReplyRequest $request, Topic $topic, Reply $reply)
+    {
+//        $reply->fill([
+//            'content' => $request->content
+//        ]);
+        $reply->fill(['content' => $request->content]);
+
+//        $reply->fill($request->only('content'));
+        $reply->topic()->associate($topic);
+        $reply->user()->associate($request->user());
+        $reply->save();
+
+        return new ReplyResource($reply);
+    }
+
+    public function destroy(Topic $topic, Reply $reply)
+    {
+        if ($reply->topic_id != $topic->id) {
+            abort(404);
+        }
+
+        $this->authorize('destroy', $reply);
+        $reply->delete();
+
+        return response(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Api\UsersController;
 use App\Http\Controllers\Api\ImagesController;
 use App\Http\Controllers\Api\CategoriesController;
 use App\Http\Controllers\Api\TopicsController;
-
+use App\Http\Controllers\Api\RepliesController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -88,6 +88,9 @@ Route::prefix('v1')
                     // 发布，修改，删除话题
                     Route::apiResource('topics', TopicsController::class)
                         ->only(['store', 'update', 'destroy']);
+                    // 发布，删除回复
+                    Route::apiResource('topics.replies', RepliesController::class)
+                        ->only(['store', 'destroy']);
                 });
             });
 
